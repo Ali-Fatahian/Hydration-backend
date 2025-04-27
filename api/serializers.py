532e5d596fa1d2531810b4b8f205d4d045a2d90d
Ali-Fattahian <ali.fattahian1378@gmail.com>
2025-04-27 23:27:42 +0200

    feat: create serializers for every model in the db

---
+++ api/serializers.py
@@ -0,0 +1,43 @@
+from django.contrib.auth import get_user_model
+from rest_framework.serializers import ModelSerializer
+from core import models
+
+User = get_user_model()
+
+class SmartBottleSerializer(ModelSerializer):
+    class Meta:
+        model = models.SmartBottle
+        fields = '__all__'
+
+
+class UserSerializer(ModelSerializer):
+    bottle = SmartBottleSerializer()
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'fullname', 'picture', 'weight',
+                  'gender', 'creatine_intake', 'date_joined', 'bottle']
+
+
+class NotificationSerializer(ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = models.Notification
+        fields = ['id', 'seen', 'date_created', 'user']
+
+
+class CreatineProductSerializer(ModelSerializer):
+    class Meta:
+        model = models.CreatineProduct
+        fields = ['id', 'company_name', 'product_name', 'picture',
+                  'price', 'discount', 'size', 'link', 'partner_id']
+
+
+class WaterConsumptionSerializer(ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = models.WaterConsumption
+        fields = ['id', 'max_water_intake', 'user_water_intake',
+                  'date_created', 'updated_at', 'user']
